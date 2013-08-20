15ff615960a9afbe7fc0c02724e97ab4c6ed8171
Jeff Miller <jmiller@enova.com>
2013-08-20 11:56:43 -0500

    Getting up and running

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,13 @@
 class Position < ActiveRecord::Base
   validates :name, uniqueness: true
   validates :abbrev, uniqueness: true
+
+  def self.go
+    Position.create(name: 'Quarterback', abbrev: 'QB')
+    Position.create(name: 'Running Back', abbrev: 'RB')
+    Position.create(name: 'Wide Reciever', abbrev: 'WR')
+    Position.create(name: 'Tight End', abbrev: 'TE')
+    Position.create(name: 'Kicker', abbrev: 'K')
+    Position.create(name: 'Defense & Special Teams', abbrev: 'DEF')
+  end
 end
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,8 +1,21 @@
 class Source < ActiveRecord::Base
-  #attr_accessor :name, :url, :weight, :site
-
   validates :name, uniqueness: true
   validates :url, uniqueness: true
   validates :weight, numericality: true
   validates :site, presence: true
+
+  def self.go
+    Source.create(name: 'Matthew Berry', 
+                  url: 'http://espn.go.com/fantasy/football/story/_/page/NFLDK2K13_Berry_200/matthew-berry-top-200-overall-fantasy-football-rankings-2013', 
+                  weight: 0.45, 
+                  site: 'ESPN')
+    Source.create(name: 'Christopher Harris', 
+                  url: 'http://espn.go.com/fantasy/football/story/_/page/NFLDK2K13_Harris_200/christopher-harris-top-200-overall-fantasy-football-rankings-2013', 
+                  weight: 0.35, 
+                  site: 'ESPN')
+    Source.create(name: 'Eric Karabell', 
+                  url: 'http://espn.go.com/fantasy/football/story/_/page/NFLDK2K13_Karabell_200/eric-karabell-top-200-overall-fantasy-football-rankings-2013', 
+                  weight: 0.2, 
+                  site: 'ESPN')
+  end
 end
