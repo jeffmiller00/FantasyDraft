d26e9fc2cf61fd30538e64980ed72697355d0bb1
Jeff Miller <jmiller@enova.com>
2014-08-28 21:01:10 -0500

    Attempt to write some tests, but it didn't quite work

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -4,4 +4,13 @@ class PlayerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'create a player from a string' do
+    p = Player.new
+    p.build_from_string 'Adrian Peterson, MIN'
+    p.position = Position.first
+    p.save
+    assert true
+  end
+
 end
